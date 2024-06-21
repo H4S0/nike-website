91b2c114915e4d9542f59b6ca96a0751cce10c98
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-06-21 10:38:07 +0200

    Add files via upload

---
+++ src/components/Nav.jsx
@@ -0,0 +1,63 @@
+import { React, useState } from "react";
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-grey"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img
+            src={hamburger}
+            alt="hamburger"
+            width={25}
+            height={25}
+            onClick={toggleMenu}
+            className="cursor-pointer"
+          />
+        </div>
+      </nav>
+      {isActive === true ? (
+        <ul className="flex flex-col items-center gap-4 mt-4 max-lg:flex">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-grey"
+                onClick={() => setIsActive(!isActive)}
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        isActive === false
+      )}
+    </header>
+  );
+};
+
+export default Nav;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import Service from "./Service";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Service,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
